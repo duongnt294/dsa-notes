31a6bd6d83b78283ceb34b4f770864f19e382155
Dương Nguyễn <theduong29042k6@gmail.com>
2026-07-23 18:43:13 +0700

    TwoSum

---
+++ leetcode/TwoSum.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class TwoSum {
+
+    public static int[] twoSum(int[] nums, int target) {
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+
+            }
+        }
+
+        return new int[]{};
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int[] nums = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        int target = sc.nextInt();
+
+        int[] ans = twoSum(nums, target);
+
+        System.out.println(ans[0] + " " + ans[1]);
+
+        sc.close();
+    }
+}
